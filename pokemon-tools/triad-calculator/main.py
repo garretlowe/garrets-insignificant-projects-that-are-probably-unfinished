7e2d02ed0c0d3182854a3c019abbf25451669383
Garret Lowe <garret.lowe98@gmail.com>
2024-06-04 17:22:03 -0400

    Naming updates for consistency

--- pokemon-tools/triad-calculator/main.py
+++ pokemon-tools/triad-calculator/main.py
@@ -8,13 +8,13 @@ def find_third_type(triads: list, perfect_triads: list, type1: str, type2: str, 
     if index >= len(TYPES[type2]['super_effective']): return
     type3 = TYPES[type2]['super_effective'][index]
     if type1 in TYPES[type3]['super_effective']:
-        type_arr = [type1, type2, type3]
-        type_arr.sort()
-        if type_arr not in triads: triads.append(type_arr)
+        final_types = [type1, type2, type3]
+        final_types.sort()
+        if final_types not in triads: triads.append(final_types)
         types_are_perfect_triad = (type3 in TYPES[type1]['not_very_effective'] and 
                                    type1 in TYPES[type2]['not_very_effective'] and 
                                    type2 in TYPES[type3]['not_very_effective'])
-        if type_arr not in perfect_triads and types_are_perfect_triad: perfect_triads.append(type_arr)
+        if final_types not in perfect_triads and types_are_perfect_triad: perfect_triads.append(final_types)
     find_third_type(triads, perfect_triads, type1, type2, index + 1)
 
 
@@ -32,16 +32,19 @@ def find_triads(triads: list, perfect_triads: list, index: int = 0) -> None:
     find_triads(triads, perfect_triads, index + 1)
 
 
+def print_lists_in_list(arr: list, index: int = 0) -> None:
+    if index >= len(arr): return
+    print(*arr[index])
+    print_lists_in_list(arr, index + 1)
+
 triads = []
 perfect_triads = []
 find_triads(triads, perfect_triads)
 
 print('Triads:')
 triads.sort()
-for triad in triads:
-	print(*triad)
+print_lists_in_list(triads)
         
 print('\nPerfect Triads:')
 perfect_triads.sort()
-for triad in perfect_triads:
-	print(*triad)
+print_lists_in_list(perfect_triads)
